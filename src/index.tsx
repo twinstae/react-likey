5474b40db831b635a7bbf3347dea99922d937465
twinstae <rabolution@gmail.com>
2021-10-12 00:53:06 +0900

    state 폴더 생성

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import {
-    BrowserRouter as Router,
-  } from "react-router-dom";
+  BrowserRouter as Router,
+} from "react-router-dom";
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('app'));
+ReactDOM.render(
+    <Router>
+      <App />
+    </Router>,
+    document.getElementById('app')
+);
