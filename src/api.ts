dafcf76f727be63fd07bf425b64bcfcbbeb1f02b
twinstae <rabolution@gmail.com>
2021-11-28 16:32:12 +0900

    api 의 반환 타입 에러 수정

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,12 @@
 
 const HOST = 'http://localhost:8000';
 
-export function getProductFromOGUrl(productLink: string): Promise<ProductT>{
+interface OGResBody {
+  title: string,
+  image: string,
+}
+
+export function getProductFromOGUrl(productLink: string): Promise<OGResBody>{
   return fetch(HOST + "/og?url="+productLink)
     .then(res => res.json())
     .catch(err => err)
