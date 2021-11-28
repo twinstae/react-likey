c329e3304047e6f999e69f614bdc0f638c927be9
twinstae <rabolution@gmail.com>
2021-11-28 16:50:35 +0900

    폰트 pretendard 전역 css

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/home';
 import "tailwindcss/tailwind.css"
+import "./app.css";
 
 const App = () => {
   return (
