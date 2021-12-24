2d86112c05540850a041a7e79904133cfe1afb82
twinstae <rabolution@gmail.com>
2021-12-24 20:00:25 +0900

    carrotframe tabs 적용

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,43 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { useState } from "react";
 import Home from "./pages/home";
+import { Tabs } from "@karrotframe/tabs";
+import "@karrotframe/tabs/index.css";
 import "tailwindcss/tailwind.css";
 import "./app.css";
+import { Toaster } from "react-hot-toast";
+
+type TabKeys = "category" | "add-product" | "community";
 
 const App = () => {
+  const [activeTabKey, setActiveTabKey] = useState<TabKeys>("category");
+
   return (
     <>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <h2 className="text-2xl text-gray-700 p-2">LIKEY</h2>
+      <Tabs
+        activeTabKey={activeTabKey}
+        tabs={[
+          {
+            key: "category",
+            buttonLabel: "카테고리",
+            render: () => <Home />,
+          },
+          {
+            key: "add-product",
+            buttonLabel: "상품 추가",
+            render: () => <div>add-product</div>,
+          },
+          {
+            key: "community",
+            buttonLabel: "커뮤니티",
+            render: () => <div>community</div>,
+          },
+        ]}
+        onTabChange={(key) => {
+          setActiveTabKey(key as TabKeys);
+        }}
+      />
+      <Toaster position="bottom-center" reverseOrder={false} />
     </>
   );
 };
