9708119650b594568b418007940322d867f28fef
twinstae <rabolution@gmail.com>
2021-12-29 19:42:51 +0900

    format code

--- src/repository/localStorageRepository.ts
+++ src/repository/localStorageRepository.ts
@@ -1,23 +1,21 @@
-import { PRODUCT_LIST_KEY } from '../constants';
-
+import { PRODUCT_LIST_KEY } from "../constants";
 
 export default function createLocalStorageRepository(): ProductRepository {
-
   // void => Product[] | null
-  function loadAllProduct(): ProductT[] | null{
+  function loadAllProduct(): ProductT[] | null {
     const save = localStorage.getItem(PRODUCT_LIST_KEY);
-    
-    if(save){
+
+    if (save) {
       return JSON.parse(save);
     }
-     
+
     return null;
   }
 
   // ProductT[] => void
-  function saveAllProduct(productList: ProductT[]): void{
+  function saveAllProduct(productList: ProductT[]): void {
     localStorage.setItem(PRODUCT_LIST_KEY, JSON.stringify(productList));
   }
-  
-  return { loadAllProduct, saveAllProduct }
+
+  return { loadAllProduct, saveAllProduct };
 }
--- src/state/bottomSheetAtom.ts
+++ src/state/bottomSheetAtom.ts
@@ -1,18 +1,18 @@
-import { atom, useAtom } from 'jotai';
+import { atom, useAtom } from "jotai";
 
 export const isOpenAtom = atom(false);
 export const productLinkAtom = atom(null as string | null);
 
-export function useBottomSheetAtom(){
+export function useBottomSheetAtom() {
   const [isOpen, setIsOpen] = useAtom(isOpenAtom);
   const [productLink, setProductLink] = useAtom(productLinkAtom);
   return {
     isOpen,
     productLink,
-    open: (newLink: string)=> {
+    open: (newLink: string) => {
       setProductLink(newLink);
       setIsOpen(true);
     },
-    dismiss: ()=>setIsOpen(false),
-  }
+    dismiss: () => setIsOpen(false),
+  };
 }
