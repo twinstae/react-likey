a38574e7731e2a415cbc1fb2aa148e8b0dc3a1e9
youngdonkim <ydkim108@gmail.com>
2022-01-25 17:49:35 +0900

    네이버 페이 구매 이력 가져오기 모듈 추가

---
+++ src/purchseScraper.ts
@@ -0,0 +1,37 @@
+import puppeteer from "puppeteer";
+import { NaverApp } from "trackpurchase";
+
+import readline from "readline";
+
+const printNaverPayHistory = async (id: string, password: string) => {
+  const MOBILE_UA =
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Mobile/15E148 Safari/604.1";
+
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--start-maximized"]
+  });
+  const page = await browser.newPage();
+  await page.setViewport({ height: 800, width: 1200 });
+  await page.setUserAgent(MOBILE_UA);
+
+  const module = NaverApp.ModuleFactory.create(page);
+  const crawlService = new NaverApp.Service(module);
+
+  await crawlService.login(id, password);
+
+  const history = await crawlService.getHistory();
+  console.log(history);
+};
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const id = "sugar9438";
+const password = "sugar9438#n";
+rl.question("Naver ID: ", (id) => {
+  rl.question("Naver Password: ", (password) => {
+    printNaverPayHistory(id, password);
+  });
+});
