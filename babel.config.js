f4115dae42e183125703e55306633cbd58735624
youngdonkim <ydkim108@gmail.com>
2022-02-04 19:30:44 +0900

    새로운 설정변경 주석처리

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: [
+//       // ['@babel/preset-env', { targets: { node: 'current' } }],
+//       ["@babel/preset-env", "@babel/preset-typescript"],
+//       "@babel/preset-typescript",
+//       "@babel/preset-react",
+//     ],
+//     plugins: ["@babel/plugin-transform-runtime"]
+//   };
+// };
+
+// {
+//   "presets": ["@babel/preset-env", "@babel/preset-typescript"],
+//   "plugins": ["@babel/plugin-transform-runtime"]
+// }
+
 module.exports = function (api) {
   api.cache(true);
   return {
