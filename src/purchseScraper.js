f538ab95de0019a820348e20313f1e81b2db2b42
youngdonkim <ydkim108@gmail.com>
2022-02-04 19:37:43 +0900

    tsc 컴파일 파일

--- src/purchseScraper.js
+++ src/purchseScraper.js
@@ -1,4 +1,6 @@
 "use strict";
+// import puppeteer from "puppeteer";
+// import { NaverApp } from "trackpurchase";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -36,18 +38,49 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 exports.__esModule = true;
+// import readline from "readline";
+// const printNaverPayHistory = async (id: string, password: string) => {
+//   const MOBILE_UA =
+//     "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Mobile/15E148 Safari/604.1";
+//   const browser = await puppeteer.launch({
+//     headless: true,
+//     args: ["--start-maximized"]
+//   });
+//   const page = await browser.newPage();
+//   await page.setViewport({ height: 800, width: 1200 });
+//   await page.setUserAgent(MOBILE_UA);
+//   const module = NaverApp.ModuleFactory.create(page);
+//   const crawlService = new NaverApp.Service(module);
+//   await crawlService.login(id, password);
+//   const history = await crawlService.getHistory();
+//   console.log(history);
+// };
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+// const id = "sugar9438";
+// const password = "sugar9438#n";
+// rl.question("Naver ID: ", (id) => {
+//   rl.question("Naver Password: ", (password) => {
+//     printNaverPayHistory(id, password);
+//   });
+// });
+// import { launch } from "puppeteer";
 var puppeteer_1 = require("puppeteer");
+// import * as puppeteer from "puppeteer";
 var trackpurchase_1 = require("trackpurchase");
-var readline_1 = require("readline");
+var readline = require("readline");
+var rxjs_1 = require("rxjs");
+// import { CaptchaStatus } from "app/naver";
 var printNaverPayHistory = function (id, password) { return __awaiter(void 0, void 0, void 0, function () {
-    var MOBILE_UA, browser, page, module, crawlService, history;
+    var MOBILE_UA, browser, page, module, crawlService, loginEvent$, history$, closePage$, closeBrowser$, final$;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
                 MOBILE_UA = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Mobile/15E148 Safari/604.1";
-                // return [4 /*yield*/, puppeteer_1["default"].launch({
-                return [4 /*yield*/, puppeteer_1.launch({
-                        headless: true,
+                return [4 /*yield*/, puppeteer_1["default"].launch({
+                        headless: false,
                         args: ["--start-maximized"]
                     })];
             case 1:
@@ -63,24 +96,49 @@ var printNaverPayHistory = function (id, password) { return __awaiter(void 0, vo
                 _a.sent();
                 module = trackpurchase_1.NaverApp.ModuleFactory.create(page);
                 crawlService = new trackpurchase_1.NaverApp.Service(module);
-                return [4 /*yield*/, crawlService.login(id, password)];
-            case 5:
-                _a.sent();
-                return [4 /*yield*/, crawlService.getHistory()];
-            case 6:
-                history = _a.sent();
-                console.log(history);
+                loginEvent$ = crawlService.interactiveLogin(id, password);
+                history$ = (0, rxjs_1.defer)(function () { return (0, rxjs_1.from)(crawlService.getHistory()); });
+                closePage$ = (0, rxjs_1.defer)(function () { return (0, rxjs_1.from)(page.close()); });
+                closeBrowser$ = (0, rxjs_1.defer)(function () { return (0, rxjs_1.from)(browser.close()); });
+                final$ = (0, rxjs_1.concat)(loginEvent$, history$, closePage$, closeBrowser$).pipe((0, rxjs_1.tap)(function (event) {
+                    if (event === "otp-required") {
+                        console.log("스마트폰 앱에서 OTP 인증을 완료해주세요.");
+                    }
+                }), (0, rxjs_1.tap)(function (event) {
+                    if (event === "manual-otp-required") {
+                        rl.question("otp code: ", function (code) { return __awaiter(void 0, void 0, void 0, function () {
+                            return __generator(this, function (_a) {
+                                module.pageInteractor.fillManualOTPInput(code);
+                                return [2 /*return*/];
+                            });
+                        }); });
+                    }
+                }), (0, rxjs_1.tap)(function (event) {
+                    function instanceOfCaptchaStatus(object) {
+                        if (object) {
+                            return "imageData" in object && "question" in object;
+                        }
+                        return false;
+                    }
+                    if (instanceOfCaptchaStatus(event)) {
+                        console.log("encodedImage: ".concat(event.imageData));
+                        console.log("question: ".concat(event.question));
+                        rl.question("captcha code: ", function (code) {
+                            module.pageInteractor.fillCaptchaInput(code, password);
+                        });
+                    }
+                }), (0, rxjs_1.filter)(function (event) { return event instanceof Array; }));
+                final$.subscribe(function (event) {
+                    console.log(event);
+                });
                 return [2 /*return*/];
         }
     });
 }); };
-// var rl = readline_1["default"].createInterface({
-var rl = readline_1.createInterface({
+var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-var id = "sugar9438";
-var password = "sugar9438#n";
 rl.question("Naver ID: ", function (id) {
     rl.question("Naver Password: ", function (password) {
         printNaverPayHistory(id, password);
